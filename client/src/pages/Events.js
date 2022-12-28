5f5e36dddc95095243f27fa35fb6d24c11eb38bb
Sabarinath <sabariavi43@gmail.com>
2022-12-28 17:56:21 +0530

    Enter into events page

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,3 +1,15 @@
+import event from "../styles/event.module.css";
+
+import { Link } from "react-router-dom";
+
 export const Events = () => {
-  return <h1>Events</h1>;
+  return (
+    <div className={event.main}>
+      <div className={event.topbar}>
+        <p>Bookly</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+      <div className={event.container}></div>
+    </div>
+  );
 };
