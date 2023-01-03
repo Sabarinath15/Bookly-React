cbcf0b5481ebd3328b31f9591e276725a5f284c3
Sabarinath15 <sabariavi53@gmail.com>
2023-01-03 16:52:22 +0530

    On profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,11 @@
 import style from "../styles/dashboard.module.css";
 
 export const Profile = () => {
-  return <div className={style.profileContainer}></div>;
+  return (
+    <div className={style.profileContainer}>
+      <img src="\assets\images\user.png" alt="" />
+      <h1>User name</h1>
+      <p>email@gamil.com</p>
+    </div>
+  );
 };
